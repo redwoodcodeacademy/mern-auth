4bbe40cd48cb19426364e35f8b52bcc6e9993000
redwoodcodeacademy <info@redwoodcodeacademy.com>
2020-12-30 15:41:45 -0800

    initial commit

---
+++ server.js
@@ -0,0 +1,28 @@
+const express = require('express')
+const mongoose = require('mongoose')
+const passport = require('passport')
+
+const users = require('./routes/api/users')
+
+const app = express();
+
+app.use(express.json());
+app.use(express.urlencoded({extended: false}));
+
+const db = require("./config/keys").mongoURI;
+mongoose
+    .connect(
+        db, 
+        { useNewUrlParser: true, useUnifiedTopology: true}
+    )
+    .then(() => console.log("MongoDB successfully connected"))
+    .catch(err => console.log(err));
+
+app.use(passport.initialize());
+
+require('./config/passport')(passport);
+
+app.use('/api/users', users);
+
+const port = process.env.PORT || 5000;
+app.listen(port, () => console.log(`Server up and running on port ${port}`));
